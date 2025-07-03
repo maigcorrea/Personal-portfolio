2b53b0d714634c3a132fad000082e9bf9231b64f
Maite <anamaitegarciacorrea@gmail.com>
2025-07-03 20:15:18 +0200

    .

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <>
         <div className='bg-blue-200 h-screen'>
           <div className=' h-screen w-full relative'>
-            <h1 className='text-7xl '>Dando forma a tus ideas</h1>
+            <h1 className='text-7xl'>Dando forma a tus ideas</h1>
             <div className='flex justify-center'>
               <div className='absolute bottom-4 w-1/4 bg-gray-300 text-white p-4 rounded-full shadow-lg'>
                     <NavBar/>
