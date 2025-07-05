0514cb794ea0ab6a6fe1be3b51963e127a547dae
Maite <anamaitegarciacorrea@gmail.com>
2025-07-05 18:01:07 +0200

    .

--- frontend/src/pages/Work.jsx
+++ frontend/src/pages/Work.jsx
@@ -10,7 +10,7 @@ const Work = () => {
     <div className="revealer"></div>
       <div className='bg-purple-200 h-screen'>
             <div className='flex justify-center h-full relative'>
-                <div className='absolute bottom-4 w-1/4 bg-gray-300 text-white p-4 rounded-full shadow-lg z-3'>
+                <div className='absolute bottom-4 bg-gray-300 text-white p-4 rounded-full shadow-lg z-3'>
                     <NavBar/>
                 </div>
             </div>
