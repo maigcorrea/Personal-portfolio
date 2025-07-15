9261af0242b88f21fd97633e1a29d8650dc6b66f
Maite <anamaitegarciacorrea@gmail.com>
2025-07-15 18:30:04 +0200

    Dither bg with ThreeJS

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import './globals.css';
 import { useRevealer } from '../hooks/useRevealer';
 import SplitText from '../hooks/splitRevealerText';
 import CurvedLoop from '../components/CurvedLoop';
+import Dither from '../components/DitherBg';
 
 const Home = () => {
   useRevealer();
@@ -72,6 +73,22 @@ const Home = () => {
 
               
             </div>
+
+
+            <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+              <Dither
+                waveColor={[0.5, 0.5, 0.5]}
+                disableAnimation={false}
+                enableMouseInteraction={true}
+                mouseRadius={0.3}
+                colorNum={4}
+                waveAmplitude={0.3}
+                waveFrequency={3}
+                waveSpeed={0.05}
+              />
+            </div>
+
+
             <div className='flex justify-center'>
               <NavBar/>
             </div>
